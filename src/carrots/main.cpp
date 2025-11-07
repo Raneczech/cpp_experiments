f3bee46b5fa4f765620490e944b45040a3341e84
Raneczech <janek.petrak@outlook.cz>
2025-11-07 21:16:23 +0100

    Update main.cpp

--- src/carrots/main.cpp
+++ src/carrots/main.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int print(const char* text);
+int println(const char* text);
+int carrots;
+
+
+int main(void){
+    while(1){
+        cout << "How many carrots do you have? " << endl;
+        cin >> carrots;
+        println("Here are two more.");
+        carrots = carrots + 2;
+        print("Now you have ");
+        cout << carrots;
+        println(" carrots.");
+        println("crunch, crunch, crunch. I ate one.");
+        carrots = carrots - 1;
+        print("Now you have ");
+        cout << carrots;
+        println(" carrots.");
+    }
+    return 0;
+}
+
+int print(const char* text){
+    cout << text;
+    return 0;
+}
+
+int println(const char* text){
+    cout << text << endl;
+    return 0;
+}
