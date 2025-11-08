ad279734ddbf15d13782a19e3666a92bc3918f82
Raneczech <janek.petrak@outlook.cz>
2025-11-08 09:37:51 +0100

    Enable error checking and update error messages
    Enabled error checking and added error count output.

--- src/error_ending/main.cpp
+++ src/error_ending/main.cpp
@@ -1,12 +1,19 @@
+//by Raneczech
+
 #include <iostream>
-bool error_checking_enabled = false;
+bool error_checking_enabled = true;
 bool error_detected = false;
+int err_count = 0;
 
 int main(void){
     using namespace std;
+
+    //here is your code
+    //use error_detected = true and err_count++ to capture an error
+    
     if(error_checking_enabled){
         if(error_detected){
-            cerr << "Error detected!" << endl;
+            cerr << err_count <<  "Errors detected!" << endl;
             return -1;
         } else {
             cout << "No errors detected." << endl;
