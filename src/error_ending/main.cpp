e1425349201ace0c92c1e7c54425869b8c7897bc
Raneczech <janek.petrak@outlook.cz>
2025-11-08 09:30:02 +0100

    Add error checking functionality in main.cpp

--- src/error_ending/main.cpp
+++ src/error_ending/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+bool error_checking_enabled = false;
+bool error_detected = false;
 
+int main(void){
+    using namespace std;
+    if(error_checking_enabled){
+        if(error_detected){
+            cerr << "Error detected!" << endl;
+            return -1;
+        } else {
+            cout << "No errors detected." << endl;
+            return 0;
+        }
+    }else{
+        cout << "Error checking is disabled." << endl;
+        return 0;
+    }
+}
